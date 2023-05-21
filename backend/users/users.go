8e01321edb158f33bff72375108737ddefbc9252
ghandic <andrewchallis@hotmail.co.uk>
2023-05-21 20:13:42 +1000

    WIP

--- backend/users/users.go
+++ backend/users/users.go
@@ -5,43 +5,37 @@ import (
 	"fmt"
 	"os"
 
-	"sync"
-
 	"github.com/bufbuild/connect-go"
+	v1 "github.com/ghandic/grpc-web-go-react-example/backend/gen/proto/users/v1"
+	"github.com/ghandic/grpc-web-go-react-example/backend/gen/proto/users/v1/usersv1connect"
 	pgx "github.com/jackc/pgx/v5"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	v1 "github.com/ghandic/grpc-web-go-react-example/backend/gen/proto/users/v1"
-	"github.com/ghandic/grpc-web-go-react-example/backend/gen/proto/users/v1/usersv1connect"
-	db "github.com/ghandic/grpc-web-go-react-example/backend/users/db"
 )
 
 type UserService struct {
 	usersv1connect.UnimplementedUserServiceHandler
-	mu    sync.Mutex
-	Conn  *pgx.Conn
-	users map[string]*v1.User
+	Conn *pgx.Conn
 }
 
-func (u *UserService) GetUser(
-	ctx context.Context,
-	req *connect.Request[v1.GetUserRequest],
-) (*connect.Response[v1.GetUserResponse], error) {
+// func (u *UserService) GetUser(
+// 	ctx context.Context,
+// 	req *connect.Request[v1.GetUserRequest],
+// ) (*connect.Response[v1.GetUserResponse], error) {
 
-	if req.Msg.UserId == "" {
-		return nil, status.Errorf(codes.InvalidArgument, "missing user id")
-	}
-	u.mu.Lock()
-	defer u.mu.Unlock()
-	user, ok := u.users[req.Msg.UserId]
-	if !ok {
-		return nil, status.Errorf(codes.NotFound, "user not found")
-	}
-	return connect.NewResponse(&v1.GetUserResponse{
-		User: user,
-	}), nil
-}
+// 	if req.Msg.UserId == "" {
+// 		return nil, status.Errorf(codes.InvalidArgument, "missing user id")
+// 	}
+// 	u.mu.Lock()
+// 	defer u.mu.Unlock()
+// 	user, ok := u.users[req.Msg.UserId]
+// 	if !ok {
+// 		return nil, status.Errorf(codes.NotFound, "user not found")
+// 	}
+// 	return connect.NewResponse(&v1.GetUserResponse{
+// 		User: user,
+// 	}), nil
+// }
 
 func (u *UserService) CreateUser(
 	ctx context.Context,
@@ -52,7 +46,7 @@ func (u *UserService) CreateUser(
 		return nil, status.Errorf(codes.InvalidArgument, "missing name")
 	}
 
-	q := db.New(u.Conn)
+	q := users.db.New(u.Conn)
 
 	pg_user, err := q.CreateUser(ctx, req.Msg.Name)
 
@@ -71,29 +65,29 @@ func (u *UserService) CreateUser(
 	}), nil
 }
 
-func (u *UserService) ListUsers(
-	ctx context.Context,
-	req *connect.Request[v1.ListUsersRequest],
-) (*connect.Response[v1.ListUsersResponse], error) {
-	u.mu.Lock()
-	defer u.mu.Unlock()
+// func (u *UserService) ListUsers(
+// 	ctx context.Context,
+// 	req *connect.Request[v1.ListUsersRequest],
+// ) (*connect.Response[v1.ListUsersResponse], error) {
+// 	u.mu.Lock()
+// 	defer u.mu.Unlock()
 
-	users := []*v1.User{}
-	for _, value := range u.users {
-		users = append(users, value)
-	}
-	return connect.NewResponse(&v1.ListUsersResponse{Users: users}), nil
-}
+// 	users := []*v1.User{}
+// 	for _, value := range u.users {
+// 		users = append(users, value)
+// 	}
+// 	return connect.NewResponse(&v1.ListUsersResponse{Users: users}), nil
+// }
 
-func (u *UserService) DeleteUser(
-	ctx context.Context,
-	req *connect.Request[v1.DeleteUserRequest],
-) (*connect.Response[v1.DeleteUserResponse], error) {
-	u.mu.Lock()
-	defer u.mu.Unlock()
+// func (u *UserService) DeleteUser(
+// 	ctx context.Context,
+// 	req *connect.Request[v1.DeleteUserRequest],
+// ) (*connect.Response[v1.DeleteUserResponse], error) {
+// 	u.mu.Lock()
+// 	defer u.mu.Unlock()
 
-	if _, ok := u.users[req.Msg.UserId]; ok {
-		delete(u.users, req.Msg.UserId)
-	}
-	return connect.NewResponse(&v1.DeleteUserResponse{}), nil
-}
+// 	if _, ok := u.users[req.Msg.UserId]; ok {
+// 		delete(u.users, req.Msg.UserId)
+// 	}
+// 	return connect.NewResponse(&v1.DeleteUserResponse{}), nil
+// }
